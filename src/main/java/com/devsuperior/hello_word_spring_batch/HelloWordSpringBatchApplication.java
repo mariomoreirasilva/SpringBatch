b4a9cc95f4974984b4c6207c515ecd17e4f3a515
mariomoreirasilva <mario@desban.org.br>
2024-12-04 11:05:12 -0300

    Projeto batch criado

---
+++ src/main/java/com/devsuperior/hello_word_spring_batch/HelloWordSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hello_word_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/hello_word_spring_batch/HelloWordSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hello_word_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
