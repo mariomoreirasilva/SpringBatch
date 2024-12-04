7eaf1290ea2bb6b701e3c688a71c5e9364dd65aa
mariomoreirasilva <mario@desban.org.br>
2024-12-04 12:42:45 -0300

    Inclusão de variável de ambiente

---
+++ src/main/java/com/devsuperior/hello_word_spring_batch/tasklet/PrintHelloTasklet.java
@@ -0,0 +1,26 @@
+package com.devsuperior.hello_word_spring_batch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class PrintHelloTasklet implements Tasklet {
+	
+	@Value("${nome}")
+	private String nome;
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("Olá " + nome + " !!!");
+		return RepeatStatus.FINISHED;
+	}
+	
+	
+
+}
